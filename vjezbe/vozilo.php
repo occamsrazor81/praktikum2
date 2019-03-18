ad5c7796752d3e494866ab5230a547499e2576b1
Knezic Ivan <knivan@pr1-08.studenti.math.hr>
2019-03-18 11:20:51 +0100

    promjena u vozilu.

--- vjezbe/vozilo.php
+++ vjezbe/vozilo.php
@@ -9,6 +9,10 @@
 <BODY>
 <?php
 
+
+
+
+
 class Vozilo
 {
   private $name;
@@ -49,9 +53,9 @@ class Vozilo
 
   function idiRavno($km)
   {
-    if($this->getDir()=='N') { $this->setY($this->getY() + $km); }
-    elseif($this->getDir()=='S') { $this->setY($this->getY() - $km); }
-    elseif($this->getDir()=='W') { $this->setX($this->getX() - $km); }
+    if(strcmp($this->getDir(),'N') === 0) { $this->setY($this->getY() + $km); }
+    elseif(strcmp($this->getDir(),'S') === 0) { $this->setY($this->getY() - $km); }
+    elseif(strcmp($this->getDir(),'W') === 0) { $this->setX($this->getX() - $km); }
     else { $this->setX($this->getX() + $km); }
 
     $this->setKilometri($this->getKilometri() + $km);
@@ -59,17 +63,17 @@ class Vozilo
 
   function skreniLijevo()
   {
-    if($this->getDir()=='N') { $this->setDir('W'); }
-    elseif($this->getDir()=='S') { $this->setDir('E'); }
-    elseif($this->getDir()=='W') { $this->setDir('S'); }
+    if(strcmp($this->getDir(),'N') === 0) { $this->setDir('W'); }
+    elseif(strcmp($this->getDir(),'S') === 0) { $this->setDir('E'); }
+    elseif(strcmp($this->getDir(),'W') === 0) { $this->setDir('S'); }
     else { $this->setDir('N'); }
   }
 
   function skreniDesno()
   {
-    if($this->getDir()=='N') { $this->setDir('E'); }
-    elseif($this->getDir()=='S') { $this->setDir('W'); }
-    elseif($this->getDir()=='W') { $this->setDir('N'); }
+    if(strcmp($this->getDir(),'N') === 0) { $this->setDir('E'); }
+    elseif(strcmp($this->getDir(),'S') === 0) { $this->setDir('W'); }
+    elseif(strcmp($this->getDir(),'W') === 0) { $this->setDir('N'); }
     else { $this->setDir('S'); }
   }
 
@@ -86,7 +90,7 @@ class Auto extends Vozilo
     $this->kilometri = 0;
   }
 
-  function potrosenBenzin() { return $this->getKilometri()*10; }
+  function potrosenBenzin() { return $this->getKilometri()/10; }
 
   function linija() { echo "Auto nema liniju.<br/>"; }
 
@@ -158,26 +162,40 @@ for($i = 0; $i < count($vozila); $i++)
   $vozila[$i]->gdjeSam();
   $r = rand(1,3);
   echo "<hr/>Random skretanje = ".$r."<br/>";
-  echo "Lijevo = 1<br/>Desno = 2<br/>Dvaput desno = 3<br/><hr/>";
+  echo "Lijevo = 1<br/>Desno = 2<br/>Dvaput desno = 3<br/>";
   if($r == 1)
   {
+    $km = rand(10,30);
     $vozila[$i]->skreniLijevo();
-    $vozila[$i]->idiRavno(rand(10,30));
+    $vozila[$i]->idiRavno($km);
+    echo "Random prijedeno kilometara = ".$km."<br/>";
   }
   elseif($r == 2)
   {
+    $km = rand(10,30);
     $vozila[$i]->skreniDesno();
-    $vozila[$i]->idiRavno(rand(10,30));
+    $vozila[$i]->idiRavno($km);
+    echo "Random prijedeno kilometara = ".$km."<br/>";
   }
 
   else
   {
+    $km = rand(10,30);
     $vozila[$i]->skreniDesno();
     $vozila[$i]->skreniDesno();
-    $vozila[$i]->idiRavno(rand(10,30));
+    $vozila[$i]->idiRavno($km);
+    echo "Random prijedeno kilometara = ".$km."<br/>";
   }
+  echo "<hr/>";
 
   $vozila[$i]->gdjeSam();
+
+  if($vozila[$i] instanceof Tramvaj)
+    echo "Tramvaj broj:".$vozila[$i]->linija()."<br/>";
+  elseif($vozila[$i] instanceof Auto)
+    echo "Auto je potrosio ".$vozila[$i]->potrosenBenzin()." litara benzina<br/>";
+
+
   echo "<hr/>";
 }
 
